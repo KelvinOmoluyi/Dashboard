a94be84225f5fda3d560a46b68e30a04179963bf
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-08-17 23:56:11 +0100

    initialized with Sidebar comp

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SideBar from '../components/SideBar'
+
+function App() {
+  return (
+    <main className='h-screen bg max-h-screen flex'>
+      <SideBar />
+    </main>
+  )
+}
+
+export default App
