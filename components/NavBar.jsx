de746f17f9258f5c5fd9a73eb72dd1112cb94564
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-08-18 20:49:56 +0100

    added avatar image and buttons

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { SearchBar } from "./index"
 import { MdNotificationsNone } from 'react-icons/md';
 import { MdOutlineAnalytics } from 'react-icons/md';
+import avatarImg from "../data/images/portfolio-about-pic.jpg"
 
 function NavBar() {
   return (
@@ -8,14 +9,14 @@ function NavBar() {
       <h2>Dashboard</h2>
       <div className="flex gap-2">
         <SearchBar />
-        <div className="size-10 bg-[#141415] rounded-full flex items-center justify-center">
+        <div className="size-10 bg-[#141415] rounded-full flex hover:cursor-pointer items-center justify-center">
           <MdNotificationsNone size={25} color="#cccccc" />
         </div>
-        <div className="size-10 bg-[#141415] rounded-full flex items-center justify-center">
+        <div className="size-10 bg-[#141415] rounded-full flex hover:cursor-pointer items-center justify-center">
           <MdOutlineAnalytics size={25} color="#cccccc" />
         </div>
-        <div className="size-10 bg-[#141415] rounded-full flex items-center justify-center">
-          <img style={{ height: 40, width: 40, borderRadius: "50%" }} src="https://kelvinportfolio01.netlify.app/img/portfolio-about-pic.jpg" alt="" />
+        <div className="size-10 bg-[#141415] hover:cursor-pointer rounded-full flex items-center justify-center">
+          <img style={{ height: 40, width: 40, borderRadius: "50%" }} src={avatarImg} alt="" />
         </div>
       </div>
     </div>
