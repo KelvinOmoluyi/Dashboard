9d390a0d15c24aa710bdaae906db25630d954b54
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-08-18 22:15:54 +0100

    made dropdown component reusable

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -1,21 +1,27 @@
 import { useState } from 'react';
 import { RiArrowDropDownLine } from 'react-icons/ri';
 import { motion } from 'framer-motion';
-import Button from './Button';
+import { Button } from './index';
 
-const Dropdown = ({ icon, text, width, height, isDropdown, dropdownData }) => {
+const Dropdown = ({ icon, width, height, text, dropdownData }) => {
+    const[isDropdown, setIsDropdown] = useState(false);
 
     const Icon = icon || null;
 
     return (
         <motion.div 
-        className="text-gray-300 hover:text-white rounded-md pl-2 gap-2"
+        className="text-gray-300 hover:text-white rounded-md gap-2"
         transition={{ duration: 0.3 }}
         >
-            <div className='w-full flex items-center justify-between'>
+            <motion.div  
+            onClick={() => setIsDropdown((prev) => !prev)}
+            style={{ height, width }}
+            className='w-full flex items-center justify-between pl-2 rounded-md'
+            whileHover={{ backgroundColor: '#9061f6' }}
+            >
                 <div className="flex gap-2">
                     {icon && <Icon size={20} className='text-gray-300 hover:text-white' />}
-                    <span>Analytics</span>
+                    <span>{text}</span>
                 </div>
                 <motion.div
                 animate={ isDropdown ? { rotate: "-90deg" } : { rotate: "0deg" }}
@@ -23,16 +29,18 @@ const Dropdown = ({ icon, text, width, height, isDropdown, dropdownData }) => {
                 >
                     <RiArrowDropDownLine size={30} />
                 </motion.div>
-            </div>
+            </motion.div>
+
             <motion.div 
-            className='w-full overflow-hidden pl-4 pt-4'
+            className='w-full overflow-hidden pl-4 pt-2'
+            initial={{ height: "0px" }}
             animate={ isDropdown ? { height: "fit-content" } : { height: "0px" }}
             transition={{ duration: 0.2 }}
             >
-                <ul className='h-fit w-full flex flex-col'>
-                    <Button text={text} width={width} height={height} />
-                    <Button text={text} width={width} height={height} />
-                    <Button text={text} width={width} height={height} />
+                <ul className='h-fit w-full flex flex-col gap-3'>
+                    {dropdownData.map((text, index) => (
+                        <Button key={index} text={text} width={width} height={height} />
+                    ))}
                 </ul>
             </motion.div>
         </motion.div>
