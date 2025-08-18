b12e1e10d2686c8b2c652eba450e8ca8bac9536a
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-08-18 22:16:38 +0100

    component index file initial commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import Button from "./Button";
+import Dropdown from "./Dropdown";
+import NavBar from "./NavBar"
+import NeutralButton from "./NeutralButton";
+import SideBar from "./SideBar";
+import SearchBar from "./SearchBar";
+
+export  { Button, Dropdown, NavBar, NeutralButton, SideBar, SearchBar }
