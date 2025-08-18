276fc84c9f7e8d66b6dc307fb4258ffa8237544d
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-08-18 22:14:45 +0100

    added active state to button

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,15 +1,15 @@
 import { motion } from "framer-motion";
 
-const Button = ({height, width, text, icon}) => {
+const Button = ({ height, width, text, icon, active }) => {
 
     const Icon = icon || null;
 
   return (
     <motion.button 
-    style={{ height, width }} 
-    className='text-gray-300 hover:text-white rounded-md flex items-center justify-start pl-2 gap-2'
+    style={width ? { height, width } : { width: "fit-content", paddingRight: 15, paddingLeft: 10 }} 
+    className={`text-gray-300 ${active && "bg-[#9061f6]"} hover:text-white rounded-md flex items-center justify-start pl-2 gap-2`}
     transition={{ duration: 0.15 }}
-    whileHover={{ backgroundColor: '#9061f6' }}
+    whileHover={{ backgroundColor: '#9061f6', cursor: "pointer" }}
     >
         {icon && <Icon size={20} className='text-gray-300 hover:text-white' />}
         <span>{text}</span>
