e1f930397bde18556c7321dde6e8ed99dca02a10
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-08-20 23:53:49 +0100

    added more props for futher customization

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,18 +1,27 @@
 import { motion } from "framer-motion";
 
-const Button = ({ height, width, text, icon, active }) => {
+const Button = ({ height, width, text, icon, bgColor, textFirst, align }) => {
 
     const Icon = icon || null;
 
   return (
     <motion.button 
     style={width ? { height, width } : { width: "fit-content", paddingRight: 15, paddingLeft: 10 }} 
-    className={`text-gray-300 ${active && "bg-[#9061f6]"} hover:text-white rounded-md flex items-center justify-start pl-2 gap-2`}
+    className={`text-gray-300 bg-[${bgColor ? bgColor : "none"}] hover:text-white rounded-md flex justify-${align === "center" ? "center" : "start"} items-center pl-2 gap-2`}
     transition={{ duration: 0.15 }}
     whileHover={{ backgroundColor: '#9061f6', cursor: "pointer" }}
     >
-        {icon && <Icon size={20} className='text-gray-300 hover:text-white' />}
-        <span>{text}</span>
+      {textFirst ? (
+        <>
+          <span>{text}</span>
+          {icon && <Icon size={20} className='text-gray-300 hover:text-white' />}
+        </>
+      ) : (
+        <>
+          {icon && <Icon size={20} className='text-gray-300 hover:text-white' />}
+          <span>{text}</span>
+        </>
+      )}
     </motion.button>
   )
 }
